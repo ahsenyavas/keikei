5163212d545b07f9cc2945899a2344f325de7bef
ahsenyavas <57hasanyavas@gmail.com>
2022-03-10 15:21:38 +0300

    loading and error screens added

--- pages/movies/index.js
+++ pages/movies/index.js
@@ -14,42 +14,47 @@ const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${key}
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=`;
 
 const YEAR_DESC = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`;
-const YEAR_ASC = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=release_date.asc&include_adult=false&include_video=false&page=1&year=2010&with_watch_monetization_types=flatrate` 
-const TITLE_ASC = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=original_title.asc&include_adult=false&include_video=false&page=1&year=2010&with_watch_monetization_types=flatrate`
-const TITLE_DESC = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=original_title.desc&include_adult=false&include_video=false&page=1&year=2010&with_watch_monetization_types=flatrate`
-
+const YEAR_ASC = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=release_date.asc&include_adult=false&include_video=false&page=1&year=2010&with_watch_monetization_types=flatrate`;
+const TITLE_ASC = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=original_title.asc&include_adult=false&include_video=false&page=1&year=2010&with_watch_monetization_types=flatrate`;
+const TITLE_DESC = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=original_title.desc&include_adult=false&include_video=false&page=1&year=2010&with_watch_monetization_types=flatrate`;
 
 export default function Movies() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [value, setValue] = useState(0);
-
-  useEffect(() => {
-    getMovies(FEATURED_API);
-  }, []);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
 
   useEffect(() => {
     if (value === 0) {
       getMovies(YEAR_DESC);
-    }
-    else if(value === 1) {
+    } else if (value === 1) {
       getMovies(YEAR_ASC);
-    }
-    else if(value === 2) {
+    } else if (value === 2) {
       getMovies(TITLE_DESC);
-    }
-    else if(value === 3) {
-    getMovies(TITLE_ASC);
+    } else if (value === 3) {
+      getMovies(TITLE_ASC);
     }
   }, [value]);
 
   const getMovies = async (API) => {
-    const response = await fetch(API);
-    const data = await response.json();
+    setIsLoading(true);
+    setHasError(false);
+    try {
+      const response = await fetch(API);
+      const data = await response.json();
 
-    console.log(data.results);
-    setMovies(data.results);
+      console.log(data.results);
+      setMovies(data.results);
+    } catch (error) {
+      setHasError(true);
+    }
+    setIsLoading(false);
   };
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
 
   const handleOnSubmit = async (e) => {
     e.preventDefault();
@@ -66,10 +71,10 @@ export default function Movies() {
   };
 
   const menuList = [
-    {value: 0, text: "Sort by year in descending order"},
-    {value: 1, text: "Sort by year in ascending order"},
-    {value: 2, text: "Sort by title in descending order"},
-    {value: 3, text: "Sort by title in ascending order"},
+    { value: 0, text: "Sort by year in descending order" },
+    { value: 1, text: "Sort by year in ascending order" },
+    { value: 2, text: "Sort by title in descending order" },
+    { value: 3, text: "Sort by title in ascending order" },
   ];
 
   const handleChange = (event) => {
@@ -77,44 +82,43 @@ export default function Movies() {
     console.log(value);
   };
 
-  
-
   return (
-    <div className={Styles.container}>
-      <h1>Populer Movies</h1>
-      <div className={Styles.head}>
-        <form onSubmit={handleOnSubmit}>
-          <input
-            type="text"
-            placeholder="Search for a movie"
-            className={Styles.search}
-            value={searchTerm}
-            onChange={handleOnChange}
-          />
-        </form>
+    <>
+      {hasError && <p>Something went wrong.</p>}
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <div className={Styles.container}>
+          <h1>Populer Movies</h1>
+          <div className={Styles.head}>
+            <form onSubmit={handleOnSubmit}>
+              <input
+                type="text"
+                placeholder="Search for a movie"
+                className={Styles.search}
+                value={searchTerm}
+                onChange={handleOnChange}
+              />
+            </form>
 
-        <div>
-          <FormControl sx={{ m: 1, width: 300 }}>
-            <InputLabel id="demo-multiple-name-label">Sort</InputLabel>
-            <Select value={value} onChange={handleChange}>
-              {menuList.map((item) => (
-                <MenuItem value={item.value}>{item.text}</MenuItem>
-              ))}
-          
-            </Select>
-          </FormControl>
+            <div>
+              <FormControl sx={{ m: 1, width: 300 }}>
+                <InputLabel id="demo-multiple-name-label">Sort</InputLabel>
+                <Select value={value} onChange={handleChange}>
+                  {menuList.map((item) => (
+                    <MenuItem value={item.value}>{item.text}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </div>
+          </div>
+          <div className={Styles.movieContainer}>
+            {movies.map((movie) => (
+              <Movie key={movie.id} {...movie} />
+            ))}
+          </div>
         </div>
-      </div>
-      <div className={Styles.movieContainer}>
-        {movies.map((movie) => (
-          <Movie key={movie.id} {...movie} />
-        ))}
-      </div>
-    </div>
+      )}
+    </>
   );
 }
-
-
-/*
-
-*/
--- pages/series/index.js
+++ pages/series/index.js
@@ -10,24 +10,44 @@ import Select from "@mui/material/Select";
 
 const key = "acc13e4b9d230b1c97ff5fdb44fb6b82";
 
-const FEATURED_API = `https://api.themoviedb.org/3/discover/tv?api_key=${key}&language=en-US&sort_by=popularity.asc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/tv?api_key=${key}&language=en-US&sort_by=popularity.asc&first_air_date_year=2010&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`;
 const SEARCH_API = `https://api.themoviedb.org/3/search/tv?api_key=${key}&language=en-US&query=`;
+
+const YEAR_DESC = `https://api.themoviedb.org/3/discover/tv?api_key=${key}&language=en-US&sort_by=first_air_date.desc&first_air_date_year=2010&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`;
+const YEAR_ASC = `https://api.themoviedb.org/3/discover/tv?api_key=${key}&language=en-US&sort_by=first_air_date.asc&first_air_date_year=2010&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`;
 
 export default function Series() {
   const [series, setSeries] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [value, setValue] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
 
   useEffect(() => {
     getSeries(FEATURED_API);
   }, []);
 
-  const getSeries = async (API) => {
-    const response = await fetch(API);
-    const data = await response.json();
+  useEffect(() => {
+    if (value === 0) {
+      getSeries(YEAR_DESC);
+    } else if (value === 1) {
+      getSeries(YEAR_ASC);
+    }
+  }, [value]);
 
-    console.log(data.results);
-    setSeries(data.results);
+  const getSeries = async (API) => {
+    setIsLoading(true);
+    setHasError(false);
+    try {
+      const response = await fetch(API);
+      const data = await response.json();
+
+      console.log(data.results);
+      setSeries(data.results);
+    } catch (error) {
+      setHasError(true);
+    }
+    setIsLoading(false);
   };
 
   const handleOnSubmit = async (e) => {
@@ -45,10 +65,10 @@ export default function Series() {
   };
 
   const menuList = [
-    {value: 0, text: "Sort by year in descending order"},
-    {value: 1, text: "Sort by year in ascending order"},
-    {value: 2, text: "Sort by title in descending order"},
-    {value:3, text: "Sort by title in ascending order"},
+    { value: 0, text: "Sort by year in descending order" },
+    { value: 1, text: "Sort by year in ascending order" },
+    { value: 2, text: "Sort by title in descending order" },
+    { value: 3, text: "Sort by title in ascending order" },
   ];
 
   const handleChange = (event) => {
@@ -56,35 +76,46 @@ export default function Series() {
   };
 
   return (
-    <div className={Styles.container}>
-      <h1>Populer Series</h1>
-      <div className={Styles.head}>
-        <form onSubmit={handleOnSubmit}>
-          <input
-            type="text"
-            placeholder="Search for a series"
-            className={Styles.search}
-            value={searchTerm}
-            onChange={handleOnChange}
-          />
-        </form>
+    <>
+      {hasError && <p>Something went wrong.</p>}
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <div className={Styles.container}>
+          <h1>Populer Series</h1>
+          <div className={Styles.head}>
+            <form onSubmit={handleOnSubmit}>
+              <input
+                type="text"
+                placeholder="Search for a series"
+                className={Styles.search}
+                value={searchTerm}
+                onChange={handleOnChange}
+              />
+            </form>
 
-        <div>
-          <FormControl sx={{ m: 1, width: 300 }}>
-            <InputLabel id="demo-multiple-name-label">Sort</InputLabel>
-            <Select value={value} onChange={handleChange}>
-              {menuList.map((item) => (
-                <MenuItem value={item.value}>{item.text}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+            <div>
+              <FormControl sx={{ m: 1, width: 300 }}>
+                <InputLabel id="demo-multiple-name-label">Sort</InputLabel>
+                <Select value={value} onChange={handleChange}>
+                  {menuList.map((item) => (
+                    <MenuItem value={item.value}>{item.text}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </div>
+          </div>
+          <div className={Styles.movieContainer}>
+            {series.map((serie) => (
+              <Movie
+                key={serie.id}
+                title={serie.name}
+                poster_path={serie.poster_path}
+              />
+            ))}
+          </div>
         </div>
-      </div>
-      <div className={Styles.movieContainer}>
-        {series.map((serie) => (
-          <Movie key={serie.id} title={serie.name} poster_path={serie.poster_path} />
-        ))}
-      </div>
-    </div>
+      )}
+    </>
   );
 }
